3c0d74652260a2897df3dc2e80d46dc38c2cd202
VIIZET <129474307+viizet@users.noreply.github.com>
2025-08-12 22:37:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ import threading
 from flask import Flask
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "")
+TOKEN = os.environ.get("TOKEN", "8271624089:AAE6A3i48GnYGjehLbKI_VwtdaEHYtQEkpY")
+API_ID = int(os.environ.get("API_ID", "26176218"))
+API_HASH = os.environ.get("API_HASH", "4a50bc8acb0169930f5914eb88091736")
 STRING = os.environ.get("STRING", "")
 
 # Validate required environment variables
